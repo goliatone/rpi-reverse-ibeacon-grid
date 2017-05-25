296688908458e05082f763eaad6a554a90e0c6c2
goliatone <burgosemi@gmail.com>
2017-05-25 17:44:51 -0400

    Remove topic subscriptions from pubsub config

--- src/config/pubsub.js
+++ src/config/pubsub.js
@@ -7,41 +7,5 @@ module.exports = {
     client: {
         url: process.env.NODE_AMQP_ENDPOINT
     },
-    channels: [
-        // {
-        //     topic: '*',
-        //     appEventType: 'janus.update',
-        //     clientEventType: 'janus.event',
-        //     description: 'Catch All Topic',
-        //     debug: false
-        // },
-        // {
-        //     topic: 'janus/*/presence',
-        //     appEventType: 'persistence.presence',
-        //     clientEventType: 'persistence.presence',
-        //     description: 'Emitted when Presence events',
-        //     debug: false
-        // },
-        // {
-        //     topic: 'janus/*/access/*',
-        //     appEventType: 'persistence.activitylog.access',
-        //     clientEventType: 'persistence.activitylog',
-        //     description: 'Emitted by Activity events',
-        //     debug: false
-        // },
-        // {
-        //     topic: 'janus/*/door/*',
-        //     appEventType: 'persistence.activitylog.door',
-        //     clientEventType: 'persistence.activitylog.door',
-        //     description: 'Emitted by Door events',
-        //     debug: false
-        // },
-        // {
-        //     topic: 'janus/credential/update',
-        //     appEventType: 'janus.credential_update',
-        //     clientEventType: 'janus.credential_update',
-        //     description: 'Updated credential',
-        //     debug: false
-        // }
-    ]
+    channels: []
 };
