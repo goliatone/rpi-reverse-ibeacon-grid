4e08266b1f3952019a0711dce8256aad44bbc7ed
goliatone <burgosemi@gmail.com>
2017-05-26 19:23:24 -0400

    Add missing uuid package

--- src/modules/pubsub/tx.ensure.uuid.js
+++ src/modules/pubsub/tx.ensure.uuid.js
@@ -1,4 +1,8 @@
+'use strict';
+
 module.exports = function(data){
-    if(!data.uuid) data.uuid = require('uuid').v4();
+    if(!data.uuid) {
+        data.uuid = require('uuid').v4();
+    }
     return data;
 };
